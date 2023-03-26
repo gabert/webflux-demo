647cebf0fb3ce68d08107d95535901923eb9d4cf
Robert Gallas <robert.gallas2@globallogic.com>
2023-03-26 20:07:25 +0200

    Example finish

--- spring-webflux-app/src/main/java/com/github/gabert/webflux/demo/app/author/controller/AuthorController.java
+++ spring-webflux-app/src/main/java/com/github/gabert/webflux/demo/app/author/controller/AuthorController.java
@@ -4,9 +4,12 @@ import com.github.gabert.webflux.demo.app.author.service.AuthorCreateRequest;
 import com.github.gabert.webflux.demo.app.author.service.AuthorDetailResponse;
 import com.github.gabert.webflux.demo.app.author.service.AuthorService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -15,7 +18,16 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @PostMapping("/api/authors")
-    public Mono<String> createAuthor(@RequestBody AuthorCreateRequest authorCreateRequest) {
-        return authorService.createAuthor(authorCreateRequest);
+    public Mono<ResponseEntity<String>> createAuthor(@RequestBody AuthorCreateRequest authorCreateRequest) {
+        return authorService.createAuthor(authorCreateRequest)
+                .map(id -> ResponseEntity.status(201)
+                                         .body(id))
+                .onErrorResume(error -> Mono.just(ResponseEntity.badRequest()
+                                                                .body(error.getMessage())));
+    }
+
+    @GetMapping("/api/authors")
+    public Flux<AuthorDetailResponse> getAllAuthors() {
+        return authorService.listAuthors();
     }
 }
